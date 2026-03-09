2daa921c2db4f0151a88d6a4d03d9e66d06c126e
blastic-cato <anabrmattos@gmail.com>
2026-03-09 14:19:32 -0300

    Added table user_profile to tracking schema.

--- sql_scripts/database_creation.sql
+++ sql_scripts/database_creation.sql
@@ -13,29 +13,40 @@ CREATE TYPE auth.color_scheme AS ENUM('DARK', 'LIGHT', 'OS');
 
 -- USER
 CREATE TABLE IF NOT EXISTS auth.user (
-    user_id uuid primary key default gen_random_uuid() not null,
-    full_name varchar(255) not null,
-    username varchar(20) unique not null,
-    role auth.user_role default 'STUDENT' not null,
-    date_of_birth timestamp not null,
-    email varchar(320) not null,
-    hash_password text not null,
-    is_private bool default false not null,
-    streak_count integer default 0 not null,
-    last_login_date timestamptz not null,
-    profile_image bytea,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null
+    user_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    role auth.user_role DEFAULT 'STUDENT' NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    hash_password TEXT NOT NULL,
+    is_private BOOL DEFAULT false NOT NULL,
+    profile_image BYTEA,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
+
+ALTER TABLE auth.user DROP COLUMN streak_count;
+ALTER TABLE auth.user DROP COLUMN last_login_date;
+
+CREATE TABLE IF NOT EXISTS tracking.user_profile (
+    user_id UUID PRIMARY KEY REFERENCES auth.user(user_id) ON DELETE CASCADE,
+    user_bio VARCHAR(400),
+    streak_count INTEGER DEFAULT 0 NOT NULL,
+    experience_points INTEGER DEFAULT 0 NOT NULL,
+    current_level INTEGER DEFAULT 1 NOT NULL,
+    last_activity_date TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS auth.user_preferences (
-    user_preferences_id uuid primary key default gen_random_uuid() not null,
-    user_id uuid not null,
-    questions_per_day integer default 10 not null,
-    exam_language content.language default 'INGLES' not null,
-    color_scheme auth.color_scheme default 'OS' not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    user_preferences_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    user_id UUID NOT NULL,
+    questions_per_day INTEGER DEFAULT 10 NOT NULL,
+    exam_language content.language DEFAULT 'INGLES' NOT NULL,
+    color_scheme auth.color_scheme DEFAULT 'OS' NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id)
         REFERENCES auth.user(user_id)
         ON DELETE CASCADE
@@ -43,13 +54,13 @@ CREATE TABLE IF NOT EXISTS auth.user_preferences (
 );
 
 CREATE TABLE IF NOT EXISTS planning.user_goal (
-    user_goal_id uuid primary key default gen_random_uuid() not null,
-    user_id uuid not null,
-    university_name varchar(255) not null,
-    course_name varchar(255) not null,
-    cut_off_score float4 not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    user_goal_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    user_id UUID NOT NULL,
+    university_name VARCHAR(255) NOT NULL,
+    course_name VARCHAR(255) NOT NULL,
+    cut_off_score FLOAT4 NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id)
         REFERENCES auth.user(user_id)
         ON DELETE CASCADE
@@ -60,18 +71,18 @@ CREATE TABLE IF NOT EXISTS planning.user_goal (
 -- SUBJECTS | SCHEDULES
 
 CREATE TABLE IF NOT EXISTS content.subject (
-    subject_id uuid primary key default gen_random_uuid() not null,
-    name content.subject_name not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null
+    subject_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    name content.subject_name NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS planning.schedule (
-    schedule_id uuid primary key default gen_random_uuid() not null,
-    user_id uuid not null,
-    name varchar(50) default 'Cronograma Semanal' not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    schedule_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    user_id UUID NOT NULL,
+    name VARCHAR(50) DEFAULT 'Cronograma Semanal' NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id)
         REFERENCES auth.user(user_id)
         ON DELETE CASCADE
@@ -79,60 +90,60 @@ CREATE TABLE IF NOT EXISTS planning.schedule (
 );
 
 CREATE TABLE IF NOT EXISTS planning.schedule_subject (
-    schedule_id uuid REFERENCES planning.schedule(schedule_id)
+    schedule_id UUID REFERENCES planning.schedule(schedule_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    subject_id uuid REFERENCES content.subject(subject_id)
+    subject_id UUID REFERENCES content.subject(subject_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
-    weekday planning.day_of_the_week not null,
-    subject_order integer not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    weekday planning.day_of_the_week NOT NULL,
+    subject_order INTEGER NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT pk_schedule_subject PRIMARY KEY (schedule_id,subject_id)
 );
 
 -- EXAMS
 
 CREATE TABLE IF NOT EXISTS tracking.exam (
-    exam_id uuid primary key default gen_random_uuid() not null,
-    user_id uuid not null,
-    title varchar(20) not null,
-    exam_year integer not null,
-    language_choice content.language not null,
-    status tracking.exam_status default 'NOT_STARTED' not null,
-    questions_count integer not null,
-    correct_questions_count integer,
-    incorrect_questions_count integer,
-    unsolved_questions_count integer,
+    exam_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    user_id UUID NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    exam_year INTEGER NOT NULL,
+    language_choice content.language NOT NULL,
+    status tracking.exam_status DEFAULT 'NOT_STARTED' NOT NULL,
+    questions_count INTEGER NOT NULL,
+    correct_questions_count INTEGER,
+    incorrect_questions_count INTEGER,
+    unsolved_questions_count INTEGER,
     total_spent_time interval,
-    max_spent_time interval not null,
-    estimated_score float4,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    max_spent_time interval NOT NULL,
+    estimated_score FLOAT4,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id)
         REFERENCES auth.user(user_id)
         ON DELETE CASCADE ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS tracking.exam_subject (
-    subject_id uuid REFERENCES content.subject(subject_id)
+    subject_id UUID REFERENCES content.subject(subject_id)
         ON DELETE SET NULL ON UPDATE NO ACTION,
-    exam_id uuid REFERENCES tracking.exam(exam_id)
+    exam_id UUID REFERENCES tracking.exam(exam_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT pk_exam_subject PRIMARY KEY (subject_id, exam_id)
 );
 
 -- QUESTION
 CREATE TABLE IF NOT EXISTS content.question (
-    question_id uuid primary key default gen_random_uuid() not null,
-    subject_id uuid,
-    posted_by_id uuid not null,
-    api_index integer not null,
-    language content.language default null,
-    content jsonb not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    question_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    subject_id UUID,
+    posted_by_id UUID NOT NULL,
+    api_index INTEGER NOT NULL,
+    language content.language DEFAULT null,
+    content jsonb NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT fk_subject_id FOREIGN KEY (subject_id)
         REFERENCES content.subject(subject_id)
         ON DELETE SET NULL ON UPDATE NO ACTION,
@@ -142,34 +153,34 @@ CREATE TABLE IF NOT EXISTS content.question (
 );
 
 CREATE TABLE IF NOT EXISTS tracking.solved_question (
-    solved_question_id uuid primary key default gen_random_uuid() not null,
-    user_id uuid not null
+    solved_question_id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID() NOT NULL,
+    user_id UUID NOT NULL
         REFERENCES auth.user(user_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    question_id uuid not null
+    question_id UUID NOT NULL
         REFERENCES content.question(question_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    question_year integer not null,
-    correct_alternative char(1) not null,
+    question_year INTEGER NOT NULL,
+    correct_alternative char(1) NOT NULL,
     chosen_alternative char(1),
-    is_correct bool not null,
-    time_spent interval not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null
+    is_correct BOOL NOT NULL,
+    time_spent interval NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tracking.exam_question (
-    exam_id uuid not null
+    exam_id UUID NOT NULL
         REFERENCES tracking.exam(exam_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    question_id uuid not null
+    question_id UUID NOT NULL
         REFERENCES content.question(question_id)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    correct_alternative char(1) not null,
+    correct_alternative char(1) NOT NULL,
     chosen_alternative char(1),
-    is_correct bool not null,
-    time_spent interval not null,
-    updated_at timestamptz default now() not null,
-    created_at timestamptz default now() not null,
+    is_correct BOOL NOT NULL,
+    time_spent interval NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     CONSTRAINT exam_question_pk PRIMARY KEY (exam_id, question_id)
 );
